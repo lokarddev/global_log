952ce93dc6823c3bcd70e78c04782ce104f373e5
Oleg Babenko <lokard.dev@gmail.com>
2022-03-18 21:32:52 +0200

    - templates ready

---
+++ templates/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="w-full flex">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+
+import Header from "@/components/Header";
+import { mapActions } from 'vuex';
+export default {
+  name: 'App',
+  components: {Header},
+  methods: mapActions(["createWSConnection", "closeWSConnection"]),
+  data() {
+    },
+  mounted() {
+    this.createWSConnection();
+  },
+  unmounted() {
+    this.closeWSConnection();
+  }
+};
+</script>
---
+++ templates/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card color="basil" class="text-center justify-center">
+    <v-card-title primary-title class="justify-center py-6 text-center" >
+      <h1 class="basil--text text-center justify-center">
+        SF LOG
+      </h1>
+    </v-card-title>
+    <v-tabs v-model="tab" background-color="basil" grow>
+      <v-tab v-for="item in list" :key="item" v-bind:to="item.to">
+        {{ item.title}}
+      </v-tab>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      tab: null,
+      list: [
+        {to: "/", title: "Dashboard"},
+        {to: "/logs", title: "Logs"},
+        {to: "/analytics", title: "Analytics"},
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .basil {
+    background-color: #FFFBE6 !important;
+  }
+  .basil--text {
+    color: #356859 !important;
+  }
+</style>
